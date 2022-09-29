e1fd2de29e6892d396b000fc3ee8a4c0b3c784af
Roni-kemp <lord.of.lamancha.r@gmail.com>
2022-09-29 15:32:28 +0300

    Update gray_thresh_chooser.py

--- loop/gray_thresh_chooser.py
+++ loop/gray_thresh_chooser.py
@@ -13,9 +13,20 @@ def nothing(x):
 
 # Load image
 image = img
-
+image =  cv.imread(r"C:\Users\Roni\Desktop\1__Croped_1\1_0016_CROPED.jpg")
 # Create a window
 cv2.namedWindow("image", cv2.WINDOW_NORMAL)
+
+# add hsv filter 
+hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+lower = np.array([ 89, 100,   0])
+upper = np.array([118, 201, 255])
+mask = cv2.inRange(hsv, lower, upper)
+image = cv2.bitwise_and(image, image, mask=mask)
+
+# cv2.imshow('image', image)
+#%
+
 
 # Create trackbars for color change
 cv2.createTrackbar('Min', 'image', 0, 255, nothing)
@@ -45,7 +56,7 @@ while(1):
     upper = np.array(Max)
 
     # Convert to grey format and threshold
-    gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+    gray = cv.cvtColor(image,cv.COLOR_BGR2GRAY)
     mask = cv2.inRange(gray, Min, Max)
     # result_grey = cv2.bitwise_and(gray, gray, mask=mask)
     result = cv2.bitwise_and(image, image, mask=mask)
