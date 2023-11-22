b3edf78807f6d0645a450f5fa48a83bb24aa9a91
Roni-kemp <lord.of.lamancha.r@gmail.com>
2023-11-22 15:41:37 +0200

    Update single_cam.py
    very basic cleanup

--- loop/single_cam.py
+++ loop/single_cam.py
@@ -20,11 +20,7 @@ L2 = 11#40
 GPIO.setup(L1, GPIO.OUT)
 GPIO.setup(L2, GPIO.OUT)
 
-def save_imgs(parent_path, f_name, width, height):
-    was_ok = True
-    ## handle folder path 
-    directory_name = "B"
-    path = os.path.join(parent_path, directory_name)
+def create_folder(path):
     ## Check if the folder exists
     ## Create empty folders if not
     if not os.path.isdir(path):
@@ -32,6 +28,17 @@ def save_imgs(parent_path, f_name, width, height):
         print("created folder")
     else:
         print("folder exists")
+
+
+def save_imgs(parent_path, f_name, width, height):
+    # init status
+    was_ok = True
+    
+    ## handle folder path 
+    directory_name = "B"
+    path = os.path.join(parent_path, directory_name)
+    
+    create_folder(path)
     
     camera = cv2.VideoCapture(0)
     
@@ -42,43 +49,35 @@ def save_imgs(parent_path, f_name, width, height):
         if ret == True:
             file_path = path + "//" + f_name +".jpg"
             cv2.imwrite(file_path, frame)
-            print(f"camera was OK")
-#             my_logging((f_name, directory_name, "worked"))
-#             time.sleep(0.5)
-            
+
         else:
             print(f"failed,{f_name} try again...")                    
 #             my_logging((f_name, directory_name, "failed"))
             was_ok = False
-            
-    
+                
     except AssertionError:
         print(f"{30*'#'}\n assertion error with video capture\n{30*'#'}")
-#         logging.info(30*'-')
-#         logging.exception('')
+        ## We should log this
         was_ok = False
         
-    
     except:
         print("error when retrying...")
-#         logging.info(30*'-')
-#         logging.exception('')
         was_ok = False
-        
-
-#     my_logging(f"{cnt} of {self.camNum} worked at {f_name}, temp was {check_temp()} deg.\n")
+        ## We should log this
+    # ADD finnaly?
     camera.release()
     return was_ok
     
-def show(ROI_dct):
+def show(ROI_dct, path):
+    ## meant to show a full img of the last capture with the ROI highlighted
+    
     cv2.destroyAllWindows()
-    my_path = "/home/pi/Desktop/agueda_imgs/__new_imgs__"
 
     ROIs = ROIs_dct["B"]
     for i in range(-1,1):
-        img_path = os.listdir(my_path + "/B")[i]
+        img_path = os.listdir(path + "/B")[i]
         
-        img = cv2.imread(my_path + f"/B/{img_path}")
+        img = cv2.imread(path + f"/B/{img_path}")
         
         for i in range(2):#ROIs:
             ROI = ROIs[i]
@@ -93,9 +92,7 @@ def show(ROI_dct):
 
 def capture_imgs():
     ## get a timestamp for the file name
-   
     now = datetime.now()
-
     curr_time = now.strftime("%m_%d__%H_%M_%S")
     print("Current Time =", curr_time)
     
@@ -110,7 +107,7 @@ def capture_imgs():
             time.sleep(2)
     print("was ok! NEXT!")
 
-# # def main():
+## def main():
 start = time.perf_counter()
 print("starting loop-", time.ctime())
 capture_imgs()
@@ -131,8 +128,4 @@ while True:
         print("last img was taken at -", time.ctime())
         lrc.loop_through_cams(ROIs_dct, light_dct)
         show(ROIs_dct)
-
-
-
-    
 
